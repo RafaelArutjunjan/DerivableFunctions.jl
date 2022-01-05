5f2b360c21671a61cbd05856c6aa4ba88200e821
Rafael Arutjunjan <arutjunjan.r@gmail.com>
2022-01-05 14:49:16 +0100

    Moved core functionality to DerivableFunctionsBase.jl
    This package now only extends the functionality of DerivableFunctionsBase.jl by loading further backends.

--- src/DerivableFunctions.jl
+++ src/DerivableFunctions.jl
@@ -1,24 +1,45 @@
 module DerivableFunctions
 
-using DataFrames
-using ForwardDiff, ReverseDiff, Zygote, FiniteDifferences
-using ModelingToolkit # To avoid error thrown in _array_for() when using Symbolics.jacobian()
-using Symbolics
+using Reexport
+using ReverseDiff, Zygote
+
+@reexport using DerivableFunctionsBase
+
+# import such that they are available downstream as if defined here
+import DerivableFunctionsBase: MaximalNumberOfArguments, KillAfter, Builder
+import DerivableFunctionsBase: _GetArgLength, _GetArgLengthOutOfPlace, _GetArgLengthInPlace
+import DerivableFunctionsBase: GetSymbolicDerivative, SymbolicPassthrough
 
 
-include("Utils.jl")
-export GetArgLength
+import DerivableFunctionsBase: suff
+suff(x::ReverseDiff.TrackedReal) = typeof(x)
+
+## rewrite
+import DerivableFunctionsBase: diff_backends
+diff_backends() = [:Symbolic, :ForwardDiff, :FiniteDifferences, :ReverseDiff, :Zygote]
+# DerivableFunctionsBase.SetDiff(vcat(deepcopy(DerivableFunctionsBase.DIFF_BACKENDS),[:ReverseDiff, :Zygote]))
+# export diff_backends
+
+import DerivableFunctionsBase: _GetDeriv, _GetGrad, _GetJac, _GetHess, _GetMatrixJac, _GetDoubleJac
+# Deriv not available for ReverseDiff
+_GetGrad(ADmode::Val{:ReverseDiff}; kwargs...) = ReverseDiff.gradient
+_GetJac(ADmode::Val{:ReverseDiff}; kwargs...) = ReverseDiff.jacobian
+_GetHess(ADmode::Val{:ReverseDiff}; kwargs...) = ReverseDiff.hessian
+# Deriv not available for Zygote
+_GetGrad(ADmode::Val{:Zygote}; order::Int=-1, kwargs...) = (Func::Function,p;Kwargs...) -> Zygote.gradient(Func, p; kwargs...)[1]
+_GetJac(ADmode::Val{:Zygote}; order::Int=-1, kwargs...) = (Func::Function,p;Kwargs...) -> Zygote.jacobian(Func, p; kwargs...)[1]
+_GetHess(ADmode::Val{:Zygote}; order::Int=-1, kwargs...) = (Func::Function,p;Kwargs...) -> Zygote.hessian(Func, p; kwargs...)
+
+import DerivableFunctionsBase: _GetDerivPass, _GetGradPass, _GetJacPass, _GetHessPass, _GetDoubleJacPass, _GetMatrixJacPass
 
 
-include("DFunctions.jl")
-export DFunction, DerivableFunction
-export EvalF, EvaldF, EvalddF, In, Out, InOut
 
+import DerivableFunctionsBase: _GetGrad!, _GetJac!, _GetHess!, _GetMatrixJac!
+_GetGrad!(ADmode::Val{:ReverseDiff}; kwargs...) = ReverseDiff.gradient!
+_GetJac!(ADmode::Val{:ReverseDiff}; kwargs...) = ReverseDiff.jacobian!
+_GetHess!(ADmode::Val{:ReverseDiff}; kwargs...) = ReverseDiff.hessian!
+_GetMatrixJac!(ADmode::Val{:ReverseDiff}; kwargs...) = _GetJac!(ADmode; kwargs...) # DELIBERATE!!!! _GetJac!() recognizes output format from given Array
 
-include("DifferentiationOperators.jl")
-export diff_backends
-export GetDeriv, GetGrad, GetJac, GetHess, GetMatrixJac, GetDoubleJac
-export GetGrad!, GetJac!, GetHess!, GetMatrixJac!
-
+import DerivableFunctionsBase: _GetGradPass!, _GetJacPass!, _GetHessPass!, _GetMatrixJacPass!
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -21,17 +21,16 @@ using SafeTestsets
         @test maximum(abs.(MatrixJac(Metric3, [5,10,15.]) - Mat)) < 1e-5
     end
 
-    for ADmode ∈ [:ForwardDiff, :Zygote, :ReverseDiff, :FiniteDiff]
+    for ADmode ∈ [:Zygote, :ReverseDiff]
         MyTest(ADmode)
     end
-
 
     function TestDoubleJac(ADmode::Symbol; kwargs...)
         DoubleJac = GetDoubleJac(ADmode; order=8, kwargs...)
         maximum(abs.(DoubleJac(x->[exp(x[1])*sin(x[2]), cosh(x[2])*x[1]*x[2]], [5,10.]) - Djac)) < 1e-5
     end
 
-    for ADmode ∈ [:ForwardDiff, :ReverseDiff, :FiniteDiff]
+    for ADmode ∈ [:ReverseDiff]
         @test TestDoubleJac(ADmode)
     end
     # Zygote does not support mutating arrays
@@ -61,108 +60,7 @@ end
         MatrixJac!(Matres, [5,10,15.]);   @test maximum(abs.(Matres - Mat)) < 1e-5
     end
 
-    for ADmode ∈ [:ForwardDiff, :Zygote, :ReverseDiff, :FiniteDiff]
+    for ADmode ∈ [:Zygote, :ReverseDiff]
         MyInplaceTest(ADmode)
-    end
-end
-
-
-@safetestset "Symbolic Passthrough" begin
-    include("SymbolicTests.jl")
-end
-
-
-@safetestset "DFunctions with symbolic derivatives (out-of-place)" begin
-    using DerivableFunctions, Test, RuntimeGeneratedFunctions
-
-    F1(x) = x^2
-    F2(x) = [exp(x), log(5sinh(x))]
-    F3(x) = [exp(x) x*log(x) sinh(cosh(x)); tanh(x) x^2 2x]
-    F4(x) = x[1]^2 + x[2]^3
-    F5(x) = [x[1]^2+x[2]^2, exp(x[2]-x[1]), log(x[1] + x[2])]
-    F6(x) = [sinh(x[3]) exp(x[1])*sin(x[2]) 0 x[2]; 0 cosh(x[2]) cos(x[2])*x[3]*x[2] x[3]]
-
-    AllSymbolic(D::DFunction) = (@test D.dF isa RuntimeGeneratedFunction; @test D.ddF isa RuntimeGeneratedFunction)
-
-    # R -> R
-    D1 = DFunction(F1; ADmode=Val(:Symbolic))
-    @test EvalF(D1, 5) == 25
-    @test EvaldF(D1, 5) == 10
-    @test EvalddF(D1, 5) == 2
-    AllSymbolic(D1)
-
-    # R -> R^n
-    D2 = DFunction(F2; ADmode=Val(:Symbolic))
-    @test EvaldF(D2, rand()) isa AbstractMatrix
-    @test EvalddF(D2, rand()) isa AbstractArray{<:Number,3}
-    AllSymbolic(D2)
-
-    # R -> R^(n×m)
-    D3 = DFunction(F3; ADmode=Val(:Symbolic))
-    @test EvalF(D3, rand()) isa AbstractMatrix
-    @test EvaldF(D3, rand()) isa AbstractArray{<:Number,3}
-    @test EvalddF(D3, rand()) isa AbstractArray{<:Number,4}
-    AllSymbolic(D3)
-
-    # R^n -> R
-    D4 = DFunction(F4; ADmode=Val(:Symbolic))
-    @test EvaldF(D4, rand(2)) isa AbstractVector
-    @test EvalddF(D4, rand(2)) isa AbstractMatrix
-    # AllSymbolic(D4)
-    # Not using Symbolics.hessian in this case since it returns wrong results
-    @test D4.dF isa RuntimeGeneratedFunction
-
-
-    #R^n -> R^m
-    D5 = DFunction(F5; ADmode=Val(:Symbolic))
-    @test EvaldF(D5, rand(2)) isa AbstractMatrix
-    @test EvalddF(D5, rand(2)) isa AbstractArray{<:Number,3}
-    AllSymbolic(D5)
-
-    # R^n -> R^(n×m)
-    D6 = DFunction(F6; ADmode=Val(:Symbolic))
-    @test EvaldF(D6, rand(3)) isa AbstractArray{<:Number,3}
-    @test EvalddF(D6, rand(3)) isa AbstractArray{<:Number,4}
-    AllSymbolic(D6)
-end
-
-@safetestset "Function Structure Inference" begin
-    using DerivableFunctions, Test
-    import DerivableFunctions: GetInOut, FindSubHyperCube
-
-    # Out-of-place
-    F1(x) = x^2
-    F2(x) = [exp(x), log(5sinh(x))]
-    F3(x) = [exp(x) x*log(x) sinh(cosh(x)); tanh(x) x^2 2x]
-    F4(x) = x[1]^2 + x[2]^3
-    F5(x) = [x[1]^2+x[2]^2, exp(x[2]-x[1]), log(x[1] + x[2])]
-    F6(x) = [sinh(x[3]) exp(x[1])*sin(x[2]) 0 x[2]; 0 cosh(x[2]) cos(x[2])*x[3]*x[2] x[3]]
-
-    @test GetInOut(F1) == (-1,-1)
-    @test GetInOut(F2) == (-1, 2)
-    @test GetInOut(F3) == (-1, (2,3))
-    @test GetInOut(F4) == ( 2,-1)
-    @test GetInOut(F5) == ( 2, 3)
-    @test GetInOut(F6) == ( 3, (2,4))
-
-    # In-place
-    F2!(y,x) = copyto!(y,[exp(x), log(5sinh(x))])
-    F3!(y,x) = y[1:2, 1:3] .= [exp(x) x*log(x) sinh(cosh(x)); tanh(x) x^2 2x]
-    F5!(y,x) = copyto!(y,[x[1]^2+x[2]^2, exp(x[2]-x[1]), log(x[1] + x[2])])
-    F6!(y,x) = y[1:2,1:4] .= [sinh(x[3]) exp(x[1])*sin(x[2]) 0 x[2]; 0 cosh(x[2]) cos(x[2])*x[3]*x[2] x[3]]
-    # copyto! fills array in column-wise order, i.e. does not respect the shape of the copied array.
-    # This means the correct size tuple cannot be inferred when using copyto! for arrays of rank ≥ 2
-
-    @test GetInOut(F2!) == (-1, 2)
-    @test GetInOut(F3!) == (-1, (2,3))
-    @test GetInOut(F5!) == ( 2, 3)
-    @test GetInOut(F6!) == ( 3, (2,4))
-
-    # Test FindSubHyperCube by filling array in random places and check that correct smallest length is determined
-    dim = 3;    max = 50;   N = 5
-    for _ in 1:3
-        locations = [reduce(vcat,[[rand(1:max)] for j in 1:dim]) for i in 1:N]
-        Z = zeros([max for i in 1:dim]...);    for loc in locations    Z[loc...] = 0.1 + rand()   end
-        @test FindSubHyperCube(Z, x->x!=0) == Tuple([maximum(getindex.(locations,i)) for i in 1:dim])
     end
 end
