6ed17b619259d489e4213e52a8197a1cf5f7c77b
Rafael Arutjunjan <arutjunjan.r@gmail.com>
2025-03-03 11:04:21 +0100

    Move code from additional backends to extension in Base

--- src/DerivableFunctions.jl
+++ src/DerivableFunctions.jl
@@ -5,24 +5,24 @@ using ReverseDiff, Zygote, FiniteDiff, FiniteDifferences
 
 @reexport using DerivableFunctionsBase
 
-# import such that they are available downstream as if defined here
-import DerivableFunctionsBase: MaximalNumberOfArguments, KillAfter, Builder
-import DerivableFunctionsBase: _GetArgLength, _GetArgLengthOutOfPlace, _GetArgLengthInPlace
-import DerivableFunctionsBase: GetSymbolicDerivative, SymbolicPassthrough
+# # import such that they are available downstream as if defined here
+# import DerivableFunctionsBase: MaximalNumberOfArguments, KillAfter, Builder
+# import DerivableFunctionsBase: _GetArgLength, _GetArgLengthOutOfPlace, _GetArgLengthInPlace
+# import DerivableFunctionsBase: GetSymbolicDerivative, SymbolicPassthrough
 
-import DerivableFunctionsBase: _GetDeriv, _GetGrad, _GetJac, _GetHess, _GetMatrixJac, _GetDoubleJac
-import DerivableFunctionsBase: _GetDerivPass, _GetGradPass, _GetJacPass, _GetHessPass, _GetDoubleJacPass, _GetMatrixJacPass
-import DerivableFunctionsBase: _GetGrad!, _GetJac!, _GetHess!, _GetMatrixJac!
-import DerivableFunctionsBase: _GetGradPass!, _GetJacPass!, _GetHessPass!, _GetMatrixJacPass!
+# import DerivableFunctionsBase: _GetDeriv, _GetGrad, _GetJac, _GetHess, _GetMatrixJac, _GetDoubleJac
+# import DerivableFunctionsBase: _GetDerivPass, _GetGradPass, _GetJacPass, _GetHessPass, _GetDoubleJacPass, _GetMatrixJacPass
+# import DerivableFunctionsBase: _GetGrad!, _GetJac!, _GetHess!, _GetMatrixJac!
+# import DerivableFunctionsBase: _GetGradPass!, _GetJacPass!, _GetHessPass!, _GetMatrixJacPass!
 
 
-import DerivableFunctionsBase: suff
-suff(x::ReverseDiff.TrackedReal) = typeof(x)
+# import DerivableFunctionsBase: suff
+# suff(x::ReverseDiff.TrackedReal) = typeof(x)
 
 ## Add new backends to the output of diff_backends()
 import DerivableFunctionsBase: AddedBackEnds
 AddedBackEnds(::Val{true}) = [:ReverseDiff, :Zygote, :FiniteDifferences, :FiniteDiff]
 
-include("DifferentiationOperators.jl")
+# include("DifferentiationOperators.jl")
 
 end
